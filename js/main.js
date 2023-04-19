d792a1e291ab89a2d203d215c4c4b32ca2ec3270
Caleb <olelecaleb176@gmail.com>
2023-04-19 14:11:41 +0300

    M Rotana

--- js/main.js
+++ js/main.js
@@ -18,21 +18,27 @@
         $(window).resize(toggleNavbarMethod);
     });
     
-    
- 
+    // Get the button
+let mybutton = document.getElementById("myBtn");
 
-    // Back to top button
-     const toTop =document.querySelector(".to-top");
-     window.addEventListener("scroll" , () => {
-if (window.pageYOffset> 100) {
-toTop.classList.add("active");
-} else{
-    toTop.classList.remove("active");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
-     })
 
-
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+ 
+     
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
